84ce15eb7a1cbbde19a0f89c80ad4fd14d83b46b
VietTho1989 <doanviettho@gmail.com>
2019-05-11 12:50:30 +0700

    Minor Update

--- Assets/Sqlite/Object/HaveDatabase/Load/File/HaveDatabaseServerLoadFileUI.cs
+++ Assets/Sqlite/Object/HaveDatabase/Load/File/HaveDatabaseServerLoadFileUI.cs
@@ -8,33 +8,33 @@ using SQLite4Unity3d;
 public class HaveDatabaseServerLoadFileUI : UIBehavior<HaveDatabaseServerLoadFileUI.UIData>
 {
 
-	#region UIData
+    #region UIData
 
-	public class UIData : HaveDatabaseServerLoadUI.UIData.State
-	{
+    public class UIData : HaveDatabaseServerLoadUI.UIData.State
+    {
 
-		public VP<string> filePath;
+        public VP<string> filePath;
 
-		#region Constructor
+        #region Constructor
 
-		public enum Property
-		{
-			filePath
-		}
+        public enum Property
+        {
+            filePath
+        }
 
-		public UIData() : base()
-		{
-			this.filePath = new VP<string>(this, (byte)Property.filePath, "");
-		}
+        public UIData() : base()
+        {
+            this.filePath = new VP<string>(this, (byte)Property.filePath, "");
+        }
 
-		#endregion
-	
-		public override Type getType ()
-		{
-			return Type.File;
-		}
+        #endregion
 
-	}
+        public override Type getType()
+        {
+            return Type.File;
+        }
+
+    }
 
     #endregion
 
@@ -52,57 +52,74 @@ public class HaveDatabaseServerLoadFileUI : UIBehavior<HaveDatabaseServerLoadFil
 
     #region Refresh
 
-    public override void refresh ()
-	{
-		if (dirty) {
-			dirty = false;
-			if (this.data != null) {
-				FileInfo fileInfo = new FileInfo (this.data.filePath.v);
-				// check directory
-				bool haveDirectory = false;
-				{
-					DirectoryInfo dir = fileInfo.Directory;
-					if (dir != null && dir.Exists) {
-						haveDirectory = true;
-					} else {
-						Debug.LogError ("Don't have directory");
-					}
-				}
-				// process
-				if (haveDirectory) {
-					// make sqliteConnection
-					SQLiteConnection connection = null;
-					{
-						// create file if not exist
-						if (!fileInfo.Exists) {
-							Mono.Data.Sqlite.SqliteConnection.CreateFile (fileInfo.FullName);
-							connection = new SQLiteConnection(fileInfo.FullName, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
-							connection.CreateTable<SqliteObject> ();
-						} else {
-							connection = new SQLiteConnection(fileInfo.FullName, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
-						}
-					}
-					// Chuyen sang load data
-					if (connection != null) {
-						HaveDatabaseServerLoadUI.UIData loadUIData = this.data.findDataInParent<HaveDatabaseServerLoadUI.UIData> ();
-						if (loadUIData != null) {
-							HaveDatabaseServerLoadDataUI.UIData loadDataUIData = new HaveDatabaseServerLoadDataUI.UIData ();
-							{
-								loadDataUIData.uid = loadUIData.state.makeId ();
-								loadDataUIData.connection.v = connection;
-							}
-							loadUIData.state.v = loadDataUIData;
-						} else {
-							Debug.LogError ("loadUIData null: " + this);
-						}
-					} else {
-						Debug.LogError ("connect null: " + this);
-						HaveDatabaseServerFailUI.UIData.changeToFail (this.data);
-					}
-				} else {
-					Debug.LogError ("Don't have directory: " + this);
-					HaveDatabaseServerFailUI.UIData.changeToFail (this.data);
-				}
+    public override void refresh()
+    {
+        if (dirty)
+        {
+            dirty = false;
+            if (this.data != null)
+            {
+                FileInfo fileInfo = new FileInfo(this.data.filePath.v);
+                // check directory
+                bool haveDirectory = false;
+                {
+                    DirectoryInfo dir = fileInfo.Directory;
+                    if (dir != null && dir.Exists)
+                    {
+                        haveDirectory = true;
+                    }
+                    else
+                    {
+                        Debug.LogError("Don't have directory");
+                    }
+                }
+                // process
+                if (haveDirectory)
+                {
+                    // make sqliteConnection
+                    SQLiteConnection connection = null;
+                    {
+                        // create file if not exist
+                        if (!fileInfo.Exists)
+                        {
+                            Mono.Data.Sqlite.SqliteConnection.CreateFile(fileInfo.FullName);
+                            connection = new SQLiteConnection(fileInfo.FullName, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+                            connection.CreateTable<SqliteObject>();
+                        }
+                        else
+                        {
+                            connection = new SQLiteConnection(fileInfo.FullName, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+                        }
+                    }
+                    // Chuyen sang load data
+                    if (connection != null)
+                    {
+                        HaveDatabaseServerLoadUI.UIData loadUIData = this.data.findDataInParent<HaveDatabaseServerLoadUI.UIData>();
+                        if (loadUIData != null)
+                        {
+                            HaveDatabaseServerLoadDataUI.UIData loadDataUIData = new HaveDatabaseServerLoadDataUI.UIData();
+                            {
+                                loadDataUIData.uid = loadUIData.state.makeId();
+                                loadDataUIData.connection.v = connection;
+                            }
+                            loadUIData.state.v = loadDataUIData;
+                        }
+                        else
+                        {
+                            Debug.LogError("loadUIData null: " + this);
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("connect null: " + this);
+                        HaveDatabaseServerFailUI.UIData.changeToFail(this.data);
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Don't have directory: " + this);
+                    HaveDatabaseServerFailUI.UIData.changeToFail(this.data);
+                }
                 // txt
                 {
                     if (lbMessage != null)
@@ -114,73 +131,80 @@ public class HaveDatabaseServerLoadFileUI : UIBehavior<HaveDatabaseServerLoadFil
                         Debug.LogError("lbMessage null");
                     }
                 }
-            } else {
-				Debug.LogError ("data null: " + this);
-			}
-		}
-	}
+            }
+            else
+            {
+                Debug.LogError("data null: " + this);
+            }
+        }
+    }
 
-	public override bool isShouldDisableUpdate ()
-	{
-		return false;
-	}
+    public override bool isShouldDisableUpdate()
+    {
+        return false;
+    }
 
-	#endregion
+    #endregion
 
-	#region implement callBacks
+    #region implement callBacks
 
-	public override void onAddCallBack<T> (T data)
-	{
-		if (data is UIData) {
+    public override void onAddCallBack<T>(T data)
+    {
+        if (data is UIData)
+        {
             // Setting
             Setting.get().addCallBack(this);
-			dirty = true;
-			return;
-		}
+            dirty = true;
+            return;
+        }
         // Setting
-        if(data is Setting)
+        if (data is Setting)
         {
             dirty = true;
             return;
         }
-        Debug.LogError ("Don't process: " + data + "; " + this);
-	}
+        Debug.LogError("Don't process: " + data + "; " + this);
+    }
 
-	public override void onRemoveCallBack<T> (T data, bool isHide)
-	{
-		if (data is UIData) {
-			UIData uiData = data as UIData;
+    public override void onRemoveCallBack<T>(T data, bool isHide)
+    {
+        if (data is UIData)
+        {
+            UIData uiData = data as UIData;
             // Setting
             Setting.get().removeCallBack(this);
-			this.setDataNull (uiData);
-			return;
-		}
+            this.setDataNull(uiData);
+            return;
+        }
         // Setting
-        if(data is Setting)
+        if (data is Setting)
         {
             return;
         }
-        Debug.LogError ("Don't process: " + data + "; " + this);
-	}
+        Debug.LogError("Don't process: " + data + "; " + this);
+    }
 
-	public override void onUpdateSync<T> (WrapProperty wrapProperty, List<Sync<T>> syncs)
-	{
-		if (WrapProperty.checkError (wrapProperty)) {
-			return;
-		}
-		if (wrapProperty.p is UIData) {
-			switch ((UIData.Property)wrapProperty.n) {
-			case UIData.Property.filePath:
-				dirty = true;
-				break;
-			default:
-				Debug.LogError ("Don't process: " + wrapProperty + "; " + this);
-				break;
-			}
-			return;
-		}
+    public override void onUpdateSync<T>(WrapProperty wrapProperty, List<Sync<T>> syncs)
+    {
+        if (WrapProperty.checkError(wrapProperty))
+        {
+            return;
+        }
+        if (wrapProperty.p is UIData)
+        {
+            switch ((UIData.Property)wrapProperty.n)
+            {
+                case UIData.Property.filePath:
+                    dirty = true;
+                    break;
+                default:
+                    Debug.LogError("Don't process: " + wrapProperty + "; " + this);
+                    break;
+            }
+            return;
+        }
         // Setting
-        if(wrapProperty.p is Setting)
+        if (wrapProperty.p is Setting)
         {
             switch ((Setting.Property)wrapProperty.n)
             {
@@ -203,9 +227,9 @@ public class HaveDatabaseServerLoadFileUI : UIBehavior<HaveDatabaseServerLoadFil
             }
             return;
         }
-        Debug.LogError ("Don't process: " + wrapProperty + "; " + syncs + "; " + this);
-	}
+        Debug.LogError("Don't process: " + wrapProperty + "; " + syncs + "; " + this);
+    }
 
-	#endregion
+    #endregion
 
 }
